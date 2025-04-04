69fe9b84e3e05ad0cf3745a26c1d07072bdfeb70
gyyst <lantianhaian@hotmail.com>
2025-04-04 11:54:32 +0800

    feat: 添加代理过滤器脚本以处理POST请求
    添加了一个新的Python脚本`proxy/filter.py`，用于从环境变量中获取URL并发送POST请求。该脚本读取`result.txt`文件内容作为请求的payload，并配置了必要的请求头。如果URL为空，脚本将抛出异常。

---
+++ proxy/filter.py
@@ -0,0 +1,45 @@
+import requests
+import os
+import sys
+from urllib.parse import urlparse
+
+# 从环境变量获取URL并解析为host和path
+url = os.getenv('SUB_URL')
+# url = "https://bair.gyyst.eu.org/Cloudflare0"
+parsed_url = urlparse(url)
+host = parsed_url.netloc
+path = parsed_url.path
+
+# 从result.txt读取内容作为payload
+with open('result.txt', 'r', encoding='utf-8') as f:
+    payload = f.read().strip()
+
+headers = {
+   'authority': host,
+   'method': ' POST',
+   'path': path,
+   'scheme': ' https',
+   'accept': ' */*',
+   'accept-language': ' zh-CN,zh;q=0.9',
+   'cache-control': ' max-age=0',
+   'origin': f'https://{host}',
+   'priority': ' u=1, i',
+   'referer': url,
+   'sec-ch-ua': ' "Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+   'sec-ch-ua-mobile': ' ?0',
+   'sec-ch-ua-platform': ' "Windows"',
+   'sec-fetch-dest': ' empty',
+   'sec-fetch-mode': ' cors',
+   'sec-fetch-site': ' same-origin',
+   'user-agent': ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+   'content-type': ' text/plain;charset=UTF-8',
+   'Host': host,
+   'Connection': 'keep-alive'
+}
+
+# 检查 URL 是否为空
+if url is None:
+    raise ValueError("URL cannot be None")
+response = requests.request("POST", str(url), headers=headers, data=payload)
+
+print(response.text)
